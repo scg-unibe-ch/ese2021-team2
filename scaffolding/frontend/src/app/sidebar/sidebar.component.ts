a618ac9df741471d10b036359df09caec47e045b
Meglil7777 <mohammad.eglil@gmx.ch>
2021-10-20 17:33:46 +0200

    first prototype of sidebar

--- scaffolding/frontend/src/app/sidebar/sidebar.component.ts
+++ scaffolding/frontend/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.model';
+
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  loggedIn: boolean | undefined;
 
-  ngOnInit(): void {
+  user: User | undefined;
+  
+
+
+  constructor(public userService: UserService) {
+    // Listen for changes
+    userService.loggedIn$.subscribe(res => this.loggedIn = res);
+    userService.user$.subscribe(res => this.user = res);
+
+    // Current value
+    this.loggedIn = userService.getLoggedIn();
+    this.user = userService.getUser();
+    console.log(this.user?.fname);
+     }
+
+  ngOnInit() {
+    this.checkUserStatus();
+  }
+
+  checkUserStatus(): void {
+    // Get user data from local storage
+    const userToken = localStorage.getItem('userToken');
+
+    // Set boolean whether a user is logged in or not
+    this.userService.setLoggedIn(!!userToken);
+
+  }
+
+  test(){
+ 
+      console.log(this.user);
+    
   }
 
 }
