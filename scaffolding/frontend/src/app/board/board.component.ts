cb078004f5796deb0a0a7857d3e1fdd98274c1b2
HannesThurnherr <hannes.thurnherr@gmail.com>
2021-10-21 19:13:59 +0200

    small changes in postlist

--- scaffolding/frontend/src/app/board/board.component.ts
+++ scaffolding/frontend/src/app/board/board.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PostListComponent } from '../post-list/post-list.component';
 import { HttpClient } from '@angular/common/http';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-board',
@@ -15,17 +17,24 @@ export class BoardComponent implements OnInit {
   
   id = 1;
 
+  
+
+  loggedIn: boolean | undefined;
+
   newTitle: string= "";
   newContent: string="";
   newSemester: string="";
   creatingPost: boolean = false;
   description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
-  constructor(httpClient: HttpClient) { 
-    this.postList = new PostListComponent(httpClient)
+  constructor(httpClient: HttpClient, public userService: UserService) { 
+    this.postList = new PostListComponent(httpClient, userService)
+    userService.loggedIn$.subscribe(res => this.loggedIn = res);
   }
 
   ngOnInit(): void {
+    
+    
   }
 
   createPost(){
