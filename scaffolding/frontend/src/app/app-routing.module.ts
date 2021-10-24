f180b6c9bc8c56a6514263c86fb5359908790a6c
Meglil7777 <mohammad.eglil@gmx.ch>
2021-10-24 19:53:37 +0200

    first implementation of routing created the following components: header, home, login, register

---
+++ scaffolding/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './board/board.component';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+
+
+export class AppRoutingModule { }
