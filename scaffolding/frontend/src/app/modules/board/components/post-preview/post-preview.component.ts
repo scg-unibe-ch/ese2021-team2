d829c5f5750540a7aa211cd3e9219dd47ae93beb
HannesThurnherr <hannes.thurnherr@gmail.com>
2021-12-05 10:40:24 +0100

    LIke on post preview Implemented

--- scaffolding/frontend/src/app/modules/board/components/post-preview/post-preview.component.ts
+++ scaffolding/frontend/src/app/modules/board/components/post-preview/post-preview.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import {Post} from 'src/app/models/post.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { UserService } from 'src/app/core/http/user.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-post-preview',
@@ -11,13 +13,17 @@ import { environment } from 'src/environments/environment';
 export class PostPreviewComponent implements OnInit {
   @Input() post: Post = new Post(0, "", "", 0, "", 0, 0, "","", "");
   likes: any  = [];
+  loggedIn = false;
+  user: User | null;
+  voted = false;
+  userCanVote= true;
 
-  constructor(public httpClient: HttpClient) { }
+  constructor(public userService: UserService,public httpClient: HttpClient) { 
+    userService.loggedIn$.subscribe(res => this.loggedIn = res);
+    userService.user$.subscribe(res => this.user = res);
+  }
 
   ngOnInit(): void {
-
-    
-
     this.httpClient.post(environment.endpointURL + "post/getLikesByPostId", {
       postId: this.post.postId
   }).subscribe((res) => {
@@ -27,9 +33,57 @@ export class PostPreviewComponent implements OnInit {
       
       this.likes = res;
       this.post.likes = this.likes.length;
+      this.canUserVote()
   }, (err: any) => {
       console.log(err);
   });
+  }
+
+  canUserVote(){  
+    this.user=this.userService.getUser();
+    if(this.user!=null){
+      this.loggedIn=true;
+    }
+    for(var i=0; i<this.post.likes; i++){  
+        if(this.likes[i].userId==this.user?.userId){
+            this.voted=true;
+        }
+    }
+  }
+
+  like(){
+   
+    
+    if(!this.voted && this.loggedIn){
+    this.post.likes++;
+    this.voted=true;
+
+    this.httpClient.post(environment.endpointURL + "user/likePost", {
+      userId: this.user?.userId,
+      postId: this.post.postId
+    }).subscribe((res) => {
+      console.log(res);
+    },(err: any) => {
+      console.log(err);
+    });
+    }
+  }
+
+  unlike(){
+    
+      if(this.voted && this.loggedIn){
+          this.post.likes--;
+          this.voted=false;
+
+          this.httpClient.post(environment.endpointURL+"post/unlike", {
+              userId: this.user?.userId,
+              postId: this.post.postId
+          }).subscribe((res) => {
+            console.log(res);
+          },(err: any) => {
+            console.log(err);
+          });
+      }
   }
 
 }
