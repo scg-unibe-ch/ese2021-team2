9c5697e3b1be098338dde61a55c8278ae82ab4ec
Meglil7777 <mohammad.eglil@gmx.ch>
2021-12-11 15:20:48 +0100

    implemented semester filter

--- scaffolding/frontend/src/app/shared/components/Pipes/filter.pipe.ts
+++ scaffolding/frontend/src/app/shared/components/Pipes/filter.pipe.ts
@@ -12,6 +12,9 @@ transform(items: any[], filter: string): any {
     }
     // filter items array, items which match and return true will be
     // kept, false will be filtered out
+    if(filter.includes("Semester")){
+      return items.filter(item => item.semester && item.semester.indexOf(filter) !== -1);
+    }
     return items.filter(item => item.category && item.category.indexOf(filter) !== -1);
 }
 }
