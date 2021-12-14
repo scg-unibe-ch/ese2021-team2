aab18eca30dafaf12a9b319bd7a71ff3d371a599
RobGibsoon <rob.gibson@gmx.net>
2021-12-14 09:54:35 +0100

    fixed bug in board

--- scaffolding/frontend/src/app/modules/board/pages/board/board.component.ts
+++ scaffolding/frontend/src/app/modules/board/pages/board/board.component.ts
@@ -53,20 +53,16 @@ export class BoardComponent implements OnInit {
     this.postList = new PostListComponent(httpClient, userService, _Activatedroute, data)
       // Listen for changes
       userService.loggedIn$.subscribe(res => this.loggedIn = res);
-      // Current value
-      this.loggedIn = userService.getLoggedIn();
-      if(this.loggedIn){
-        this.user = userService.getUser();
-      }
-
-    if(this.loggedIn){
-    httpClient.post(environment.endpointURL + "board/isUserNotSubscribed", {
-      boardId: this.id,
-      userId: userService.getUser()?.userId
-    }).subscribe((res: any) => {
-     this.unsubscribed=res
-    })
-    }
+      userService.user$.subscribe(res => {
+          httpClient.post(environment.endpointURL + "board/isUserNotSubscribed", {
+              boardId: this.id,
+              userId: userService.getUser()?.userId
+          }).subscribe((res: any) => {
+              this.unsubscribed=res;
+          });
+          this.loggedIn= userService.getLoggedIn();
+          this.user=userService.getUser();
+      })
   }
 
   ngOnInit(): void {
